0cc72abc579a1889a285ad95278638891f274263
Lucas Pires <LucasP@leapthought.co.nz>
2022-08-25 22:02:27 -0300

    more problems

---
+++ icecream-parlor/main.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    check_respectives = {}
+    for idx, a in enumerate(arr):
+        respective = m - a
+        if respective in check_respectives.keys():
+            if check_respectives[respective] != idx:
+                result = sorted([idx + 1, check_respectives[respective] + 1])
+                return result
+        else:
+            check_respectives[a] = idx
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
